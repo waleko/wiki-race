b26367ada0ae760e278f4d4e4a26de06647ca258
Alex Kovrigin <a.kovrigin0@gmail.com>
2021-11-29 23:12:25 +0300

    try heroku ci

---
+++ wiki_parser/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase, RequestFactory
+
+
+# Create your tests here.
+from wiki_parser.views import parse_wiki_page
+
+
+class ParserTests(TestCase):
+    def test_simple_parsing(self):
+        request = RequestFactory().get('/')
+        response = parse_wiki_page(request, "London")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("London", response.content.__str__())
