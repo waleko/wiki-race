88c44b6e7b680c40b0d36d3eb3af2b8c27a9e8a7
Alex Kovrigin <a.kovrigin0@gmail.com>
2021-11-29 23:26:54 +0300

    try heroku ci with black

--- wiki_parser/tests.py
+++ wiki_parser/tests.py
@@ -7,7 +7,7 @@ from wiki_parser.views import parse_wiki_page
 
 class ParserTests(TestCase):
     def test_simple_parsing(self):
-        request = RequestFactory().get('/')
+        request = RequestFactory().get("/")
         response = parse_wiki_page(request, "London")
 
         self.assertEqual(response.status_code, 200)
