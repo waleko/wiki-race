e888160994d9427dc5149337ffc3e039c631cfce
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-09-02 21:12:29 +0000

    fix render port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 python manage.py migrate
 python manage.py createcachetable
-python -m gunicorn wiki_race.asgi -k uvicorn.workers.UvicornWorker
+python -m gunicorn wiki_race.asgi -k uvicorn.workers.UvicornWorker -b ":${PORT:-3001}"
